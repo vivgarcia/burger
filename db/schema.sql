5234af4ac4119d201e606a5da8530897378e0ab4
vivgarcia <viv.garcia88@gmail.com>
2020-04-30 14:13:21 -0500

    Adding schema.sql and seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -4,5 +4,7 @@ USE burgers_db;
 CREATE TABLE burgers (
     id INTEGER AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(60) NOT NULL,
-    devoured BOOLEAN 
+    devoured BOOLEAN,
+    PRIMARY KEY (id)
+    
 )
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers(burger_name, devoured)
+VALUES("rodeo goat's white animal" , false);
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES("shake shack's shackburger" , false);
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES("chris madrid's macho cheddar cheese" , false);

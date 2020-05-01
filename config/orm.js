0f5f8a216319d93c89378d5181fefd39b690e217
vivgarcia <viv.garcia88@gmail.com>
2020-05-01 15:57:47 -0500

    added orm methods

--- config/orm.js
+++ config/orm.js
@@ -44,6 +44,57 @@ var orm = {
             }
             cb(result);
         })
+    },
+    // Add a burger to the db.
+    insertOne: function(table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function(err, result) {
+            if (err) {
+                throw err
+            }
+            cb(result);
+        });
+    },
+    // Devours da burger
+    updateOne: function(table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err
+            }
+            cb(result);
+        });
+    },
+    // Delete da burger from the database
+    deleteOne: function(table, condition, cb) {
+        var queryString = "DELETE FROM " + table;
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err
+            }
+            cb(result);
+        });
     }
 }
 module.exports = orm;

d9e8ee6d6bd3f187c15c69f925bff958438db9ad
vivgarcia <viv.garcia88@gmail.com>
2020-05-01 15:37:16 -0500

    added helper functions printQuestionMarks and objToSql

--- config/connection.js
+++ config/connection.js
@@ -2,20 +2,20 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Sc4rlet008",
-  database: "burgers_db"
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Sc4rlet008",
+    database: "burgers_db"
 });
 
 // Make connection.
 connection.connect(function(err) {
-  if (err) {
+    if (err) {
     console.error("error connecting: " + err.stack);
     return;
-  }
-  console.log("connected as id " + connection.threadId);
+    }
+    console.log("connected as id " + connection.threadId);
 });
 
 // Export connection for our ORM to use.

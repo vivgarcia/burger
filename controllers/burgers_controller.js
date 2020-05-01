e6c0b33540565674f71d23eda69cb2f7542aea9b
vivgarcia <viv.garcia88@gmail.com>
2020-05-01 17:01:35 -0500

    working on routes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,7 +4,42 @@ var router = express.Router();
 
 // Create the router for the app, and export the router at the end of your file.
 var burger = require("../models/burger.js");
+// Create routes and logic witin those routes where required
+router.get("/", function(req, res){
+    burger.selectAll(function(data){
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+// Add new burger to the db
+router.post("/api/burgers", function(req, res){
+    burger.insertOne([
+        "burger_name", "devoured"
+    ], [
+        req.body.burger_name, req.body.devoured
+    ], function(result){
+        res.json({id: result.insertId});
+    });
+});
+// set devoured status to true
+router.put("/api/burgers/:id", function(req, res){
+    var condition = "id = " + req.params.id;
 
+    console.log("condition", condition);
+
+    burger.updateOne({ 
+        devoured: req.body.devoured 
+    }, condition, function(result){
+        if(result.changedRows === 0){
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
 
 
 

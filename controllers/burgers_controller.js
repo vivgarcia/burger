00f0008c69ec5b69eac9fd426eb6c7eb75b557be
vivgarcia <viv.garcia88@gmail.com>
2020-05-01 16:40:10 -0500

    added burger.js methods

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,12 @@
+// Dependencies
+var express = require("express");
+var router = express.Router();
+
+// Create the router for the app, and export the router at the end of your file.
+var burger = require("../models/burger.js");
+
+
+
+
+
+module.exports = router;
